434df4d4f0c2dec507c4eda39b416a861d7aa208
uday180900 <59023250+uday180900@users.noreply.github.com>
2025-05-20 18:36:01 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          hello updated checking...
+          Hi
         </p>
       </div>
       <p className="read-the-docs">
